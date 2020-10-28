d0b20caee78d845a1fdaefdba23cd7d2475b27a7
Kaze Shindo <kaze.bknb@outlook.jp>
2020-10-28 21:10:07 +0900

    create Delete ShellScript

---
+++ delete.sh
@@ -0,0 +1,36 @@
+#!bin/bash
+
+# 2
+rm 2-gitlogsCSV/*
+rm 2-gitlogsTXT/*
+rm 2-list-to-gitshow/*
+
+rm 2-error-gitlogs.txt
+touch 2-error-gitlogs.txt
+
+# 3
+rm 3-pastfiles/*
+rm 3-error-filenotExists.txt
+
+touch 3-error-filenotExists.txt
+
+# 4
+rm 4-rowComments/*
+
+# 5
+rm 5-satdComments/*
+
+rm satd-keywords.txt
+touch satd-keywords.txt
+
+# 6
+rm 6-satdCommentsAddInfo/*
+
+rm 6-error-addInfo.txt
+touch 6-error-addInfo.txt
+
+# 7
+rm 7-satdCommnetsUNIQUE/*
+
+# 8
+rm 8-allprojectCSV/*
