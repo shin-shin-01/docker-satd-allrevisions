75e7feb55e7e2711ca08f72373459ff06eafe7a6
Kaze Shindo <kaze.bknb@outlook.jp>
2020-11-18 23:21:13 +0900

    rm no-longer-used func

--- a-getUniqueSATD.py
+++ a-getUniqueSATD.py
@@ -5,7 +5,6 @@ import re
 from setting import PATH_OF_ALLPROJECT_CSV_UPDATE
 
 
-
 def get_unique_satd_toCSV(df):
     df[["Comment"]].drop_duplicates().to_csv("./a-extractComment/uniqueALLSATD.csv")
 
@@ -24,17 +23,7 @@ def get_unique_satd_with_deleteInfo_toCSV(df):
     tmp = tmp[["Comment"]].drop_duplicates().to_csv("./a-extractComment/SATDDeleted.csv")
 
 
-def get_azuma_count(df):
-    """
-    プロジェクトごとに SATD　のユニークを取得している
-    """
-    df = df[df["コメント追加からコメント削除までの日数"] == "削除されていません"]
-    df = df[df["LatestDockerfile"].apply(lambda filename: filename[-10:] == "Dockerfile")]
-
-    print("東さんのデータに合わせた整形→行数 : ", len(df[["project", "Comment"]].drop_duplicates()))
-
 if __name__ == "__main__":
     df = pd.read_csv(f'{PATH_OF_ALLPROJECT_CSV_UPDATE}/result.csv', index_col=0)
     get_unique_satd_toCSV(df)
     get_unique_satd_with_deleteInfo_toCSV(df)
-    get_azuma_count(df)
